21757fe551d4ac30f814a557900d1d27886c2c42
Rafael Antonio Sotelo Oviedo <86132634+Raso11@users.noreply.github.com>
2025-06-27 20:01:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
